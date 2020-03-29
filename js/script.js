7a140fb9d30fa6e674e5737775eb463540f5aec4
Raj Tyagi <rajtyagi2718@gmail.com>
2020-03-29 01:10:36 -0400

    cell states. todo: keypad

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-// face up: opacity 1, number showing
-// face down: opacity .33, number not showing, border variable
+// keypad widget
+
 const cellStop = 11;
 
 const colorArray = [
@@ -8,6 +8,8 @@ const colorArray = [
   '#764000', '#E19E57', '#010570', '#3266B2', '#B8F7FE',
   '#FFFFFF', '#000000',
 ];
+
+const frameArray = ['no-frame', 'small-frame', 'medium-frame', 'all-frame'];
 
 function textCellRowCol(i, j) {
   let text;
@@ -35,12 +37,42 @@ for (let i = 0; i < cellStop; i++) {
   for (let j = 0; j < cellStop; j++) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
-    cell.dataset.row = i.toString();
-    cell.dataset.col = j.toString();
-    cell.style['background-color'] = colorArray[i];
+    cell.id = i.toString() + j.toString();
     cell.innerHTML = textCellRowCol(i, j);
-    if (j % 2) {
-      cell.style['opacity'] = 0.33;
+    cell.dataset.score = 0;
+    cell.style['border-color'] = colorArray[i]; 
+    cell.style['background-color'] = colorArray[i];
+    cell.classList.add('back');
+    if (i && j) {
+      cell.classList.add('no-frame');
+    } else {
+      cell.classList.add('guide');
     }
     grid.appendChild(cell);
 }};
+
+document.getElementById('1010').classList.toggle('three-digits');
+
+function increaseScore(cell) {
+  let score = parseInt(cell.dataset.score);
+  if (score < 3) {
+    score++;
+  }
+  cell.dataset.score = score;
+  cell.classList.toggle(frameArray[score]);
+};
+
+function selectCell(cell) {
+  if (cell.classList.toggle('is-selected') && !(cell.classList.contains('guide'))) {
+    cell.classList.toggle(frameArray[parseInt(cell.dataset.score)]);
+  }
+  if (cell.classList.toggle('back') && !(cell.classList.contains('guide'))) {
+    increaseScore(cell);
+}};
+
+grid.addEventListener('click', function (event) {
+  let clicked = event.target;
+  if (clicked.nodeName === 'SECTION') { return; }
+  selectCell(clicked);
+});
+
